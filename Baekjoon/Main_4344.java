515f648e68e77e76e95f823b63a306bd0079c89c
Yong <98104967+kunzatt@users.noreply.github.com>
2024-08-13 22:27:19 +0900

    [BOJ] 평균은 넘겠지 / 브론즈 1
    https://www.acmicpc.net/problem/4344

---
+++ Baekjoon/Main_4344.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_4344 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int C = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < C; i++) {
+			double sum = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int[] arr = new int[N];
+			for (int j = 0; j < N; j++) {
+				arr[j] = Integer.parseInt(st.nextToken());
+				sum += arr[j];
+			}
+			double avg = sum/N;
+			double count = 0;
+			for (int j = 0; j < N; j++) {
+				if(arr[j] > avg) {
+					count++;
+				}
+			}
+			double result = (count/N)*100;
+			System.out.printf("%.3f%%\n",result); //소수점 3번째 자리까
+		}
+	}
+}
