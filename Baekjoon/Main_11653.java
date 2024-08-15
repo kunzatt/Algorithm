20119dddd3fddf15a37135634b77d7c26f7d8565
Yong <98104967+kunzatt@users.noreply.github.com>
2024-08-15 22:27:23 +0900

    [BOJ] 소인수분해 / 브론즈 1
    https://www.acmicpc.net/problem/11653

---
+++ Baekjoon/Main_11653.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_11653 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		primeNumber(N);
+
+	}
+	
+	static void primeNumber(int n) {
+		int[] arr = new int[n+1];
+		
+		for (int i = 2; i <= n; i++) {
+			arr[i] = i;
+		}
+		
+		for (int i = 2; i <= n; i++) {
+			if(arr[i] == 0) continue;
+			for (int j = i+i; j <= n; j+=i) {
+				arr[j] = 0;
+			}
+		}
+		while(n>1) {
+			for (int i = 2; i <= n; i++) {
+				if(arr[i] == 0) continue;
+				if((n%arr[i]) == 0) {
+					n = n/arr[i];
+					System.out.println(arr[i]);
+					break;
+				}				
+			}
+		}
+	}
+}
