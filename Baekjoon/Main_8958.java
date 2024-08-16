7d4608d59183a78498117c874f2c64f591c73d48
Yong <98104967+kunzatt@users.noreply.github.com>
2024-08-16 10:21:17 +0900

    [BOJ] OX퀴즈 / 브론즈 1
    https://www.acmicpc.net/problem/8958

---
+++ Baekjoon/Main_8958.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main_8958 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		String[] str = new String[N];
+		
+		for (int i = 0; i < N; i++) {
+			str[i] = br.readLine();
+		}
+		
+		for (int i = 0; i < str.length; i++) {
+			int sum = 0;
+			int count = 1;
+			for (int j = 0; j < str[i].length(); j++) {
+				char[] ch = new char[str[i].length()];
+				ch[j] = str[i].charAt(j);
+				if(ch[j] == 'O') {
+					sum += count++;
+				}else if(ch[j] == 'X') {
+					count = 1;
+				}
+			}
+			System.out.println(sum);
+		}
+	}
+}
