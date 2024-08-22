04643da8109044fd323d6ad693791268f279ff4c
Yong <98104967+kunzatt@users.noreply.github.com>
2024-08-22 16:53:55 +0900

    [BOJ] N과 M (4) / 실버 3
    https://www.acmicpc.net/problem/15652

---
+++ Baekjoon/Main_15652.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main_15652 {
+	
+	static int N, M;
+	static int[] arr;
+	static int[] nums;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		nums = new int[M];
+		for (int i = 0; i < N; i++) {
+			arr[i] = i+1;
+		}
+		dfs(0, 0);
+		System.out.println(sb);
+	}
+
+	static void dfs(int cnt, int start) {
+		if(cnt == M) {
+			for (int i = 0; i < M; i++) {
+				sb.append(nums[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			nums[cnt] = arr[i];
+			dfs(cnt+1, i);
+		}
+	}
+
+}
