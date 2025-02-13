23b55ea71a4006d05d7d744fcc1c9de713783509
Yong <98104967+kunzatt@users.noreply.github.com>
2025-02-13 22:47:13 +0900

    [Silver II] Title: 주식, Time: 936 ms, Memory: 307460 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.java
+++ 백준/Silver/11501. 주식/주식.java
@@ -3,40 +3,31 @@ import java.util.*;
 
 public class Main {
 
-    private static int start, end, N;
-    private static long sum;
-
     public static void main(String[] args) throws IOException{
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
         for(int t = 0; t < T; t++) {
-            N = Integer.parseInt(br.readLine());
+            int N = Integer.parseInt(br.readLine());
             int[] arr = new int[N];
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
 
-            sum = 0;
-            start = 0;
-            while (start < N) {
-                end = start;
-                for (int i = start; i < N; i++) {
-                    if (arr[i] >= arr[end]) {
-                        end = i;
-                    }
-                }
+            long ans = 0;
+            int max = 0;
 
-                for (int i = start; i < end; i++) {
-                    sum += arr[end] - arr[i];
+            for (int i = N - 1; i >= 0; i--) {
+                if (arr[i] >= max) {
+                    max = arr[i];
+                }else {
+                    ans += max - arr[i];
                 }
-
-                start = end + 1;
             }
 
-            System.out.println(sum);
+            System.out.println(ans);
 
         }
 
