0a6afe9ebc2b8cca5396a7051a6ef832bc6b3797
Yong <98104967+kunzatt@users.noreply.github.com>
2025-02-17 21:44:00 +0900

    [Silver I] Title: 팔, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/1105. 팔/팔.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String first = st.nextToken();
+        String second = st.nextToken();
+
+        int count = 0;
+
+        if (first.length() == second.length()){
+            for (int i = 0; i < second.length(); i++) {
+                if (second.charAt(i) != first.charAt(i)) break;
+                else {
+                    if (first.charAt(i) == '8') count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+
+
+
+    }
+}
