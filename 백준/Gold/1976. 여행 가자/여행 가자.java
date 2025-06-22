a7a739f8f4f1538905ad364689efc01351d45695
Yong <98104967+kunzatt@users.noreply.github.com>
2025-06-22 14:22:22 +0900

    [Gold IV] Title: 여행 가자, Time: 208 ms, Memory: 17224 KB -BaekjoonHub

---
+++ 백준/Gold/1976. 여행 가자/여행 가자.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        
+        int[][] arr = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        for (int i = 1; i <= n; i++) {
+            arr[i][i] = 1;
+        }
+        
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (arr[i][k] == 1 && arr[k][j] == 1) {
+                        arr[i][j] = 1;
+                    }
+                }
+            }
+        }
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] plan = new int[m];
+        for (int i = 0; i < m; i++) {
+            plan[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        boolean ok = true;
+        for (int i = 1; i < m; i++) {
+            if (arr[plan[i-1]][plan[i]] == 0) {
+                ok = false;
+                break;
+            }
+        }
+        
+        System.out.println(ok ? "YES" : "NO");
+    }
+}
