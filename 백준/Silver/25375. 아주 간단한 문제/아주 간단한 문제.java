12b802e18fa7c82cc121328a5715390f1101b6eb
Yong <98104967+kunzatt@users.noreply.github.com>
2025-10-27 22:03:25 +0900

    [Silver V] Title: 아주 간단한 문제, Time: 308 ms, Memory: 46464 KB -BaekjoonHub

---
+++ 백준/Silver/25375. 아주 간단한 문제/아주 간단한 문제.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int q = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        while (q-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            sb.append(a*2<=b&&b%a==0?1:0).append('\n');
+        }
+        System.out.println(sb);
+    }
+}
